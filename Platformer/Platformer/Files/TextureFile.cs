35f5e5e7ec1778c464d53a561d86715133d0fb78
Tetrergeru <ArutunoffOleg@yandex.ru>
2019-03-16 21:02:45 +0300

    Added Data class

--- Platformer/Platformer/Files/TextureFile.cs
+++ Platformer/Platformer/Files/TextureFile.cs
@@ -1,7 +1,22 @@
-﻿namespace Platformer.Files
+﻿using System;
+using viper_script;
+
+namespace Platformer.Files
 {
     public class TextureFile
     {
-        
+        private static TextureAnimated GetTextureAnimated(Data data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static ITexture GetTexture(string fname)
+        {
+            var data = Interpreter.Interpret(fname);
+            if (data.GetString("type") == "TextureAnimated")
+                return GetTextureAnimated(data);
+
+            throw new NotImplementedException();
+        }
     }
 }
--- Platformer/viper-script/Interpreter.cs
+++ Platformer/viper-script/Interpreter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text.RegularExpressions;
@@ -8,18 +9,45 @@ using viper_script.CodeBlocks;
 
 namespace viper_script
 {
+    public class Data
+    {
+        private Dictionary<string, Container> Variables { get; }
+
+        internal Data(Dictionary<string, Container> variables)
+        {
+            Variables = variables;
+        }
+
+        public string GetString(string index)
+        {
+            if (Variables[index].value is string str)
+                return str;
+            throw new Exception("Значение было не string");
+        }
+
+        public int GetInt(string index)
+        {
+            if (Variables[index].value is int x)
+                return x;
+            throw new Exception("Значение было не int");
+        }
+    }
+
     public static class Interpreter
     {
-        public static Dictionary<string, Container> Interpret(List<string> code)
+        public static Data Interpret(List<string> code)
         {
             var translator = new Translator(code);
             var core = translator.Translate();
             core.Interpret();
             if (core is NamespaceBlock block)
-                return block.Variables;
+                return new Data(block.Variables);
 
             throw new Exception("Результатом трансляции был не корневой блок");
         }
+
+        public static Data Interpret(string fname)
+            => Interpret(File.ReadAllLines(fname).ToList());
 
         internal static Container Interpret(ICodeBlock context, MultiTreeNode<Value> line)
         {
