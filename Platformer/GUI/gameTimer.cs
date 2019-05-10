db4c8951649f28f5cbf85db0e68b50e996619936
Tetrergeru <ArutunoffOleg@yandex.ru>
2019-05-10 12:54:04 +0300

    Added new timer

---
+++ Platformer/GUI/gameTimer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using Platformer.Game;
+
+namespace GUI
+{
+    class GameTimer : ITimer
+    {
+        private bool _running;
+
+        readonly Thread _thread;
+
+        private Action<double> _eventActions;
+        public GameTimer()
+        {
+            _thread = new Thread(TimerThread);
+            _thread.Start();
+        }
+
+        private void TimerThread()
+        {
+            double deltatime = 0;
+            while (true)
+            {
+                var startTime = DateTime.Now;
+                if (_running)
+                {
+                    _eventActions(deltatime / 1000);
+                }
+                deltatime = DateTime.Now.Subtract(startTime).TotalMilliseconds;
+            }
+        }
+
+        public void Start()
+        {
+            _running = true;
+        }
+
+        public void Stop()
+        {
+            _running = false;
+        }
+
+        public void AddEvent(Action<double> eventAction)
+        {
+            _eventActions += eventAction;
+        }
+    }
+}
--- Platformer/Platformer/Program.cs
+++ Platformer/Platformer/Program.cs
@@ -4,9 +4,11 @@ namespace Platformer
 {
     public static class Platformer
     {
-        public static IGame CreateGame()
+        public static IGame CreateGame(ITimer timer = null)
         {
-            return new Game.Game(new Game.Timer(10));
+            if (timer == null)
+                timer = new Timer(10);
+            return new Game.Game(timer);
         }
     }
 }
